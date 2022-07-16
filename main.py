da37331114da9a119e20d0d7419160eca0ea3f1e
Gaifutdinov Ivan <eiwvan@yahoo.com>
2022-07-16 12:46:43 +1000

    nxt

--- main.py
+++ main.py
@@ -2,5 +2,22 @@
 
 arr = [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0]
 
-print(len(set(arr)))  # count
-print(set(arr))  # elements
+result = []
+
+i = 0
+prev = arr[i]
+i += 1
+counter = 1
+while i != len(arr):
+    nxt = arr[i]
+
+    if nxt != prev:
+        result.append([prev, counter])
+        counter = 1
+    else:
+        counter += 1
+    prev = nxt
+    i += 1
+if arr[-2] != nxt:
+    result.append([nxt, 1])
+print(result)
